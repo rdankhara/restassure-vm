f241d177dd3a62c421eeca804e2032c5fe1cbe32
rtdankhara <rajnikant.dankhara@gmail.com>
2022-08-01 20:26:47 +0530

    extracted failing cases

--- src/test/java/marvel/MarvelCharacterTests.java
+++ src/test/java/marvel/MarvelCharacterTests.java
@@ -2,6 +2,7 @@ package marvel;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import org.hamcrest.core.Is;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
 
 public class MarvelCharacterTests {
     @BeforeClass
@@ -20,40 +22,46 @@ public class MarvelCharacterTests {
     }
 
     @Test
-    public void verify_character_properties(){
+    public void verify_character_properties_1(){
 
-        Response response = getResponse();
+        ValidatableResponse response = getResponse();
 
-        //JsonPath results = response.jsonPath().getJsonObject("data.results[0].id");
-        Assert.assertThat(response.getStatusCode(), Is.is(200));
-        String actualId = response.jsonPath().getString("data.results[0].id");
-        String expectedId = "82967";
-        Assert.assertEquals(expectedId, actualId);
+        String jsonPathExpression = "data.results[0]";
+            response.assertThat()
+                .body(jsonPathExpression, hasKey("id"))
+                .body(jsonPathExpression, hasKey("description"))
+                .body(jsonPathExpression, hasKey("modified"))
+                .body(jsonPathExpression, hasKey("resourceURI"))
+                .body(jsonPathExpression, hasKey("thumbnail"))
+                .body(jsonPathExpression, hasKey("stories"))
+                .body(jsonPathExpression, hasKey("events"))
+                .body(jsonPathExpression, hasKey("urls"));
+    }
 
-        //part two $.phoneNumbers[?(@.type=='home')].number
-        // this will load all url into an array
-        System.out.println(response.jsonPath().getList("data.results[0].urls", Detail.class));
-        List<Detail> details = response.jsonPath().getList("data.results[0].urls", Detail.class);
+    @Test
+    public void verify_character_properties_2_failing(){
 
-        for (Detail a : details){
-            if (a.getUrl().contains(expectedId)) {
-                System.out.println(a.getUrl());
-            }
-        }
+        ValidatableResponse response = getResponse();
 
-        System.out.println("---- Pretty print response ---");
-        response.getBody().prettyPrint();
+        String jsonPathExpression = "data.results[0]";
+
+        response.assertThat()
+                .body(jsonPathExpression, hasKey("comics"))
+                .body(jsonPathExpression, hasKey("name"));
+
     }
 
     @Test
     public void verify_character_count(){
 
-        Response response = getResponse();
+        Response response = getResponse().extract().response();
+
 
         //JsonPath results = response.jsonPath().getJsonObject("data.results[0].id");
         Assert.assertThat(response.getStatusCode(), Is.is(200));
-        String actualId = response.jsonPath().getString("data.results[0].id");
+        String actualId = response.jsonPath().getString("data.results[0].id1");
         String expectedId = "82967";
+
         Assert.assertEquals(expectedId, actualId);
 
         int expectedCount = response.jsonPath().getInt("data.count");
@@ -70,20 +78,20 @@ public class MarvelCharacterTests {
         }
 
         for (String url : urls) {
+            System.out.println(url);
             //Todo: write webdriver methods to visit open url and count characters, it should be equal to expectedCount
         }
     }
 
-    private Response getResponse() {
-        Response response = given()
+    private ValidatableResponse getResponse() {
+        ValidatableResponse response = given()
                 .queryParam("ts", "abcd") // any string
                 .queryParam("apikey", "3750536889898a0565ac71a0eb6920ad") // public key
                 .queryParam("hash", "9df35ef657b719ae8195f0b7adc5d995") // md5 hash
                 .when()
                 .get()
-                .then()
-                .extract()
-                .response();
+                .then();
+
         return response;
     }
 }
